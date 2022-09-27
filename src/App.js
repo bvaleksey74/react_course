042d6ceeaf21a145c830f74e39b2aed46d1104df
bvaleksey74 <bvaleksey74@yandex.ru>
2022-09-27 13:10:37 +0500

    hw4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import ChatsPage from "./pages/ChatsPage";
+import ProfilePage from "./pages/ProfilePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route path={'/'} element={<MainLayout/>}>
+              <Route index element={<HomePage/>}/>
+              <Route path={'chats'} element={<ChatsPage/>}/>
+              <Route path={'profile'} element={<ProfilePage/>}/>
+              <Route path={'*'} element={<NotFoundPage/>}/>
+          </Route>
+      </Routes>
     </div>
   );
 }
