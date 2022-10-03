b264e8caa8d4d610ca1566c16251fc390eff084e
Aleksey <bvaleksey74@yandex.ru>
2022-10-03 16:57:41 +0500

    hw6

--- src/index.js
+++ src/index.js
@@ -3,12 +3,50 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
+const initialChats = {
+    id1: {
+        name: "Chat1",
+        messages: [{id: 0, text: "FirstMessage", author: 'BOT' }],
+    },
+    id2: {
+        name: "Chat2",
+        messages: [{id:0, text: "FirstMessageHereToo!", author: 'ME' }],
+    },
+};
+
+const reducer = (state = initialChats, action) => {
+    const chats = {...state}
+    switch (action.type) {
+        case 'ADD_CHAT':
+            return {...state, [action.payload.id]: {name: action.payload.name, messages: []}}
+        case 'DEL_CHAT':
+            delete chats[action.payload]
+            return chats
+        case 'ADD_MESSAGE':
+            const id = action.payload.id
+            const message = action.payload.message
+            chats[id].messages.push(message)
+            return chats
+        case 'DEL_MESSAGE':
+            const chat = chats[action.payload.id]
+            return {...state, [action.payload.id]: {
+                name: chat.name,
+                messages: chat.messages.filter((message) => message.id !== action.payload.messageId)}}
+        default :
+            return state
+    }
+}
+const store = createStore(reducer)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,24 +1,42 @@
 import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
-import {Chats} from "../messages";
+import {useDispatch, useSelector} from "react-redux";
 
 const ChatPage = () => {
+    const [author, setAuthor] = useState('')
+    const [message, setMessage] = useState('')
     const {id} = useParams()
-    const findChat = (id) =>{
-        const chat = chats.find((item) => {return item.id == id})
-        return chat.messages;
+    const dispatch = useDispatch()
+    const chatsList = useSelector(state => state)
+
+    const addMessage = (id) => {
+        dispatch({type: 'ADD_MESSAGE', payload: {id, message: {id: Date.now(), text: message, author: author}}})
     }
-    const chats = Chats;
-    const [messages, setMessages] = useState([...findChat(id)])
+    const delMessage = (messageId) => {
+        dispatch({type: 'DEL_MESSAGE', payload: {id: id, messageId}})
+    }
     return (
         <div>
-            {messages.map((message)=>{
-                return (
-                    <div key={message.id}>
-                        {message.author}: {message.text}
-                    </div>
-                )
-            })}
+            {chatsList[id].messages.map(
+                chat => {
+                    return (
+                        <div key={chat.id}>
+                            {chat.author}: {chat.text}
+                            <button onClick={() => {
+                                delMessage(chat.id)
+                            }}>x
+                            </button>
+                        </div>
+                    )
+                }
+            )
+            }
+            <input placeholder={'author'} type="text" onChange={event => setAuthor(event.target.value)}/>
+            <input placeholder={'message'} type="text" onChange={event => setMessage(event.target.value)}/>
+            <button onClick={() => {
+                addMessage(id)
+            }}>Send
+            </button>
         </div>
     );
 };
